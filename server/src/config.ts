e59c6b6fdcf0a0a26ee930c2ad699fd0b7585383
James Duong <james@q-83.com>
2021-06-02 20:58:37 +1000

    server - initial commit with boilerplate

---
+++ server/src/config.ts
@@ -0,0 +1,7 @@
+export const CONFIG = {
+  API_URL: "http://localhost:4000",
+  CLIENT_URL: "http://localhost:3000",
+  MONGO_URL: "mongodb://localhost:27017",
+  MONGO_DB_NAME: "gym-time-dev",
+  PORT: 4000,
+};
---
+++ server/src/server.ts
@@ -0,0 +1,50 @@
+require("dotenv").config();
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import Express from "express";
+import mongoose from "mongoose";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/user";
+import jwt from "jsonwebtoken";
+import { UserModel } from "./entities/User";
+import { CONFIG } from "./config";
+
+const main = async () => {
+  try {
+    await mongoose.connect(CONFIG.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: CONFIG.MONGO_DB_NAME,
+    });
+
+    const schema = await buildSchema({
+      resolvers: [UserResolver],
+    });
+
+    const apolloServer = new ApolloServer({ schema });
+
+    const app = Express();
+
+    app.get("/confirmation/:token", async (req, res) => {
+      try {
+        const {
+          user: { _id },
+        } = jwt.verify(req.params.token, process.env.EMAIL_SECRET) as any; //tofix
+        await UserModel.update({ isEmailValidated: true }, { where: { _id } });
+      } catch (e) {
+        res.send("error");
+      }
+
+      return res.redirect(`${CONFIG.CLIENT_URL}/successful`);
+    });
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(CONFIG.PORT, () => {
+      console.log(`server started on ${CONFIG.API_URL}/graphql`);
+    });
+  } catch (ex) {
+    console.log("main EXCEPTION", ex);
+  }
+};
+
+main();
